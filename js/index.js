887d57b3364de016af574923e688244bb51a9697
DumitruBolandau78 <dumitruwebdev@gmail.com>
2022-10-31 02:00:52 +0200

    update

--- js/index.js
+++ js/index.js
@@ -9,3 +9,31 @@ function menuClose() {
     document.getElementById('aside-menu-open').style.display = 'block';
     document.getElementById('aside-menu-close').style.display = 'none';
 }
+
+function product1() {
+    document.getElementById('modal').style.display = 'flex';
+    document.getElementById('option1').selected = 'selected';
+    document.getElementById('modal-title').innerHTML = 'Семена';
+}
+
+function product2() {
+    document.getElementById('modal').style.display = 'flex';
+    document.getElementById('option2').selected = 'selected';
+    document.getElementById('modal-title').innerHTML = 'СЗР';
+}
+
+function product3() {
+    document.getElementById('modal').style.display = 'flex';
+    document.getElementById('option3').selected = 'selected';
+    document.getElementById('modal-title').innerHTML = 'Удобрения';
+}
+
+function product4() {
+    document.getElementById('modal').style.display = 'flex';
+    document.getElementById('option4').selected = 'selected';
+    document.getElementById('modal-title').innerHTML = 'Техника';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none'
+}
